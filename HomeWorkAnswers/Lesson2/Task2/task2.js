7323fe57a3748409f1088804b8269a405ab9e297
Vlad Rutskiy <vladrytskiy@gmail.com>
2024-07-17 16:47:34 -0600

    Added home work folder

---
+++ HomeWorkAnswers/Lesson2/Task2/task2.js
@@ -0,0 +1,9 @@
+
+    // document.querySelector("#btn").onclick = function() {
+    //     let output = document.queryselector("#output");
+    // output.Task2.html = new Date ();
+// }
+// document.querySelector("#btn").onclick = function() {
+//     let output = document.queryselector("#output");
+//     output. innerHIML = "HELLo, my name 15 Onytro";
+// }
