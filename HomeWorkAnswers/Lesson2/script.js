8c6228e48da0b5b58e8bf7c305d62c94b392f08f
Sasha Bezdolia <bezdolya.aleksandr@gmail.com>
2024-07-24 19:53:06 +0300

    lesson2 homework first commit

---
+++ HomeWorkAnswers/Lesson2/script.js
@@ -0,0 +1,27 @@
+const myFirstName = 'Sasha';
+const myLastName = 'Bezdolia';
+
+const outputGreetings = document.querySelector('#greetingsToUser');
+const outputCurrentDate = document.querySelector('#currentDate');
+const buttonShowGreetings = document.querySelector('#showGreetingsToUser');
+const buttonShowCurrentDate = document.querySelector('.showCurrentDate');
+
+
+buttonShowGreetings.onclick = (() => {
+
+  if(myFirstName && myLastName) {
+    outputGreetings.innerHTML = `Hello ${myFirstName} ${myLastName}`;
+  } else {
+    outputGreetings.innerHTML = `Hello new user`;
+  }
+
+  buttonShowGreetings.style.display = 'none';
+})
+
+
+buttonShowCurrentDate.onclick = (() => {
+
+  outputCurrentDate.innerHTML = 'Current date is ' + new Date();
+  buttonShowCurrentDate.style.display = 'none';
+
+})
