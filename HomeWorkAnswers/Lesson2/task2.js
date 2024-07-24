075f7db03ca89927f01739d70912a9b3a3345191
Lia <pavlova_li@ukr.net>
2024-07-24 19:57:51 +0300

    hw-2 compleate

---
+++ HomeWorkAnswers/Lesson2/task2.js
@@ -0,0 +1,18 @@
+document.querySelector("#btn").onclick = function(){
+    let output = document.querySelector("#output");
+    output.innerHTML = new Date();
+    output.style.color = "green";
+}
+
+const welcomeForm = document.querySelector('.js-welcomeForm');
+
+const handleSubmit = (evt) => {
+  evt.preventDefault();
+  const form = evt.target;
+  const name = form.elements.name.value;
+
+  alert(`Привіт ${name || 'незнайомцю'}`);
+  form.reset();
+};
+
+welcomeForm.addEventListener('submit', handleSubmit);
