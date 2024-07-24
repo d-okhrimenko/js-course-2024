1a60413792fc73080443b5a6df6676587211be6b
Sasha Bezdolia <bezdolya.aleksandr@gmail.com>
2024-07-24 20:00:15 +0300

    hotfix homework lesson2

--- HomeWorkAnswers/Lesson2/script.js
+++ HomeWorkAnswers/Lesson2/script.js
@@ -8,20 +8,16 @@ const buttonShowCurrentDate = document.querySelector('.showCurrentDate');
 
 
 buttonShowGreetings.onclick = (() => {
-
   if(myFirstName && myLastName) {
     outputGreetings.innerHTML = `Hello ${myFirstName} ${myLastName}`;
   } else {
     outputGreetings.innerHTML = `Hello new user`;
   }
-
   buttonShowGreetings.style.display = 'none';
 })
 
 
 buttonShowCurrentDate.onclick = (() => {
-
   outputCurrentDate.innerHTML = 'Current date is ' + new Date();
   buttonShowCurrentDate.style.display = 'none';
-
 })
