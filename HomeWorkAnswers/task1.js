17323a8c222fbdb3cdb7e4390f0c96f202f3f3d7
olvak <82042917+olvak@users.noreply.github.com>
2024-07-25 14:22:18 +0300

    task1 update

--- HomeWorkAnswers/task1.js
+++ HomeWorkAnswers/task1.js
@@ -3,15 +3,14 @@ function checkInput() {
     while (true) {
         inputValue = Number(prompt("Input number"));
         if (isNaN(inputValue)) {
-            alert('It is not a NUMBER. Please, input NUMBER!');
-        } else {
-            if (inputValue % 2 == 0) {
-                alert("Number " + inputValue + " is even.");
-            } else {
-                alert("Number " + inputValue + " is odd.");
-            }
-            break;
+            alert("It is not a NUMBER. Please, input NUMBER!");
+            continue;
         }
+        if (inputValue % 2 === 0) {
+            alert("Number " + inputValue + " is even.");
+            continue;
+        }
+        alert("Number " + inputValue + " is odd.");
     }
 }
 
