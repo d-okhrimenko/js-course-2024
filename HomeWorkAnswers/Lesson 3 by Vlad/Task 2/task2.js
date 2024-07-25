94ca27e8898b008118dd1ba984059ccb3b2808cf
Vlad Rutskiy <vladrytskiy@gmail.com>
2024-07-25 10:43:41 -0600

    Horray, the task 2 is done!

--- HomeWorkAnswers/Lesson 3 by Vlad/Task 2/task2.js
+++ HomeWorkAnswers/Lesson 3 by Vlad/Task 2/task2.js
@@ -1,6 +1,34 @@
 document.querySelector("#btn").onclick = function () {
     let date = new Date();
-    let outputDate = `Current date is: ${date}`
-    document.querySelector("#output").innerHTML = outputDate;
-    //console.log(date);
+    let dayNum = Number(date.getDay());
+
+    if (dayNum == 0) {
+        let outputDay = `Current day is: Sunday`;
+        document.querySelector("#output").innerHTML = outputDay;
+    }
+    else if (dayNum == 1) {
+        let outputDay = `Current day is: Monday`;
+        document.querySelector("#output").innerHTML = outputDay;
+    }
+    else if (dayNum == 2) {
+        let outputDay = `Current day is: Tueasday`;
+        document.querySelector("#output").innerHTML = outputDay;
+    }
+    else if (dayNum == 3) {
+        let outputDay = `Current day is: Wednesday`;
+        document.querySelector("#output").innerHTML = outputDay;
+    }
+    else if (dayNum == 4) {
+        let outputDay = `Current day is: Thursday`;
+        document.querySelector("#output").innerHTML = outputDay;
+    }
+    else if (dayNum == 5) {
+        let outputDay = `Current day is: Friday`;
+        document.querySelector("#output").innerHTML = outputDay;
+    }
+    else if (dayNum == 6) {
+        let outputDay = `Current day is: Satarday`;
+        document.querySelector("#output").innerHTML = outputDay;
+    }
+
 }
