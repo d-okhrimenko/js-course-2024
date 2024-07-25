64e651279435edbeeb1af54ce2ee2d231dab7f3f
Vlad Rutskiy <vladrytskiy@gmail.com>
2024-07-25 09:50:12 -0600

    one more not working changes

--- HomeWorkAnswers/Lesson 3 by Vlad/Task 2/task2.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("#btnDay").onclick = function() {
-    let day = new Date();
-    showResult (day);
-}
-function showResult(day) 
-        {
-            let output = document.querySelector("#output");
-            output.innerHTML = result;
-        }
