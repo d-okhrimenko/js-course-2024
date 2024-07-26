354cd7a2f571c31e1da8e02c00c4e4c4e2b8c557
Olya Yatsenko <olyacenko1@gmail.com>
2024-07-26 16:11:46 +0300

    corrections to the Lesson2 added

--- HomeWorkAnswers/Lesson2/file.js
+++ HomeWorkAnswers/Lesson2/file.js
@@ -1,17 +1,17 @@
 //  2.Створіть сторінку яка виводить на екран поточну дату. Наступний JS код виводить поточну дату - new Date()
- 
+
 function receiveDate() {
     let today = new Date();
-    let currentDate = today.getFullYear() + "-" + (today.getMonth()+1) + "-" + today.getDate();
+    let currentDate = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
     let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
-    document.querySelector("#btnDate").click = alert("Date: " + currentDate + "\n" + "Time: " + time);
+    alert("Date: " + currentDate + "\n" + "Time: " + time);
 };
 
 // 3.Створіть кнопку на сторінці з текстом "Скажи привіт", зробіть щоб при кліку по кнопці виводилося повідомлення "Привіт {name}" де замість {name} буде виводитися ваше ім'я.
 
 function sayName() {
     let name = "Olya";
-    
-    document.querySelector("#btnName").click = alert("Hello, " + name + "!");
+
+    alert("Hello, " + name + "!");
 };
