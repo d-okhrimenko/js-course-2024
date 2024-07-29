3eed45cc5d5e1dcf73d88fccfd826a487e01fe92
Oksana Baranik <ksyubaranik@gmail.com>
2024-07-29 08:52:42 +0200

    Finished task 3 of Lesoon 3

--- HomeWorkAnswers/Lesson 3/script.js
+++ HomeWorkAnswers/Lesson 3/script.js
@@ -13,27 +13,39 @@
 // }
 
 // Task 2
-let todayDate = document.getElementById('todayDate');
-let btnCheck = document.getElementById('btnCheck');
-let output = document.getElementById('output');
+// let todayDate = document.getElementById('todayDate');
+// let btnCheck = document.getElementById('btnCheck');
+// let output = document.getElementById('output');
 
-btnCheck.onclick = function() {
-    todayDate = new Date().getDay();
-    if (todayDate === 0) {
-        output.innerHTML = "Today is Sunday";
-    } else if (todayDate === 1) {
-            output.innerHTML = "Today is Monday";
-    } else if (todayDate === 2) {
-        output.innerHTML = "Today is Tuesday";
-    } else if (todayDate === 3) {
-            output.innerHTML = "Today is Wednesday";
-    } else if (todayDate === 4) {
-        output.innerHTML = "Today is Thursday";
-    } else if (todayDate === 5) {
-            output.innerHTML = "Today is Friday";
+// btnCheck.onclick = function() {
+//     todayDate = new Date().getDay();
+//     if (todayDate === 0) {
+//         output.innerHTML = "Today is Sunday";
+//     } else if (todayDate === 1) {
+//             output.innerHTML = "Today is Monday";
+//     } else if (todayDate === 2) {
+//         output.innerHTML = "Today is Tuesday";
+//     } else if (todayDate === 3) {
+//             output.innerHTML = "Today is Wednesday";
+//     } else if (todayDate === 4) {
+//         output.innerHTML = "Today is Thursday";
+//     } else if (todayDate === 5) {
+//             output.innerHTML = "Today is Friday";
+//     } else {
+//         output.innerHTML = "Today is Saturday";
+//     }   
+// }
+
+// Task 3
+let currentYear = prompt("Put current year");
+currentYear = parseInt(currentYear);
+
+if (!isNaN(currentYear)) {
+    if (currentYear % 4 === 0 && currentYear % 100 !== 0 || currentYear % 400 === 0) {
+        alert ("This year is a leap year");
     } else {
-        output.innerHTML = "Today is Saturday";
-    }   
+        alert ("This year is not a leap year");
+    }
+} else {
+    alert ("Put the correct year");
 }
-
-
