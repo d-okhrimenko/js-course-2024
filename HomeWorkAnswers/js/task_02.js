02aab4d51ec166520a22c85b91ae5e73ff5f97bd
slavon213 <slavon213@gmail.com>
2024-07-30 12:03:10 +0300

    task 02 show day of the week

--- HomeWorkAnswers/js/task_02.js
+++ HomeWorkAnswers/js/task_02.js
@@ -1,3 +1,19 @@
 function WhichDay() {
-    
+
+    let today = new Date().getDay();
+    if (today === 0) {
+        alert("Сьогодні неділя");
+    } else if (today === 1) {
+        alert("Сьогодні понеділок");
+    } else if (today === 2) {
+        alert("Сьогодні вівторок");
+    } else if (today === 3) {
+        alert("Сьогодні середа");
+    } else if (today === 4) {
+        alert("Сьогодні четвер");
+    } else if (today === 5) {
+        alert("Сьогодні п'ятниця");
+    } else if (today === 6) {
+        alert("Сьогодні субота");
+    }
 };
