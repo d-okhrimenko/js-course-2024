369030f64d4d0a21f1bbbed9b07a491e2c621910
Tetiana Andrushchenko <andryshchenkotanya@gmail.com>
2024-07-31 00:34:08 +0300

    added task2 and task3 to the homework3

--- HomeWorkAnswers/Lesson3/task2-scripts.js
+++ HomeWorkAnswers/Lesson3/task2-scripts.js
@@ -1,56 +1,51 @@
 // Завдання 2
 // Написати програму, яка визначає, який сьогодні день тижня, і виводить відповідне повідомлення.
 
-// Кроки для виконання завдання:
-
-// Отримання поточної дати:
-// - Використайте об'єкт Date для отримання поточної дати.
-
-// Визначення дня тижня:
-// - Використайте метод getDay() об'єкта Date для отримання дня тижня (0 для неділі, 1 для понеділка і т.д.).
-
-// Виведення результату:
-// - Використайте оператор if для визначення дня тижня і виведення відповідного повідомлення.
 //Варіант 1
-// let now = new Date();
-// let options = { weekday: "long" };
-// let weekdayDisplay = new Intl.DateTimeFormat("uk-UA", options).format(now);
 
-// function showCurrentWeekday() {
-//   let current_weekday = document.querySelector("#current_weekday");
-//   current_weekday.innerHTML = "Сьогодні " + weekdayDisplay + ".";
-// }
-// showCurrentWeekday();
+let now = new Date();
+let options = { weekday: "long" };
+let weekdayDisplay = new Intl.DateTimeFormat("uk-UA", options).format(now);
+
+function showCurrentWeekday() {
+  let current_weekday = document.querySelector("#current_weekday");
+  current_weekday.innerHTML = "Сьогодні " + weekdayDisplay + ".";
+}
+showCurrentWeekday();
+
 //Варіант 2
+
 const currentDate = new Date();
 let dayOfWeek = currentDate.getDay();
-if ((dayOfWeek = 0)) {
-  weekdayName = "Неділя";
+console.log(currentDate);
+console.log(dayOfWeek);
+
+if (dayOfWeek == 0) {
+  weekdayName = "неділя";
 } else {
-  if ((dayOfWeek = 1)) {
-    weekdayName = "Понеділок";
+  if (dayOfWeek == 1) {
+    weekdayName = "понеділок";
   } else {
-    if ((dayOfWeek = 2)) {
-      weekdayName = "Вівторок";
+    if (dayOfWeek == 2) {
+      weekdayName = "вівторок";
     } else {
-      if ((dayOfWeek = 3)) {
-        weekdayName = "Середа";
+      if (dayOfWeek == 3) {
+        weekdayName = "середа";
       } else {
-        if ((dayOfWeek = 4)) {
-          weekdayName = "Четвер";
+        if (dayOfWeek == 4) {
+          weekdayName = "четвер";
         } else {
-          if ((dayOfWeek = 5)) {
-            weekdayName = "П'ятниця";
+          if (dayOfWeek == 5) {
+            weekdayName = "п'ятниця";
           } else {
-            weekdayName = "Субота";
+            weekdayName = "субота";
           }
         }
       }
     }
   }
 }
-console.log(currentDate);
-console.log(dayOfWeek);
+
 function showCurrentWeekdayV2() {
   let current_weekday_v2 = document.querySelector("#current_weekday_v2");
   current_weekday_v2.innerHTML = "Сьогодні " + weekdayName + ".";
