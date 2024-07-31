863bfd8d2d2a520ac65c720c20f127134743e39c
Oleksandr <mario880001@gmail.com>
2024-07-31 18:47:44 +0300

    added hw task3 not full

---
+++ HomeWorkAnswears/Lesson 3/js/myhw3_task3.js
@@ -0,0 +1,10 @@
+// let year = +prompt("Який зараз рік на дворі?)")
+// console.log(typeof(year));
+
+let yearNow = prompt("Який зараз рік на дворі?)")
+yearNow = Number(yearNow);
+console.log(typeof(yearNow));
+
+alert(new Date().getFullYear());
+
+// З цим завдання поки не розібрався, зроблю пізніше.
