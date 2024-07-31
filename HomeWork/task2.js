8a6deef142bbf3da7be83093be893a57a02e33db
Andrey Urusov <urusov1402@gmail.com>
2024-07-31 06:49:20 +0300

    andriy-lesson-3

--- HomeWork/task2.js
+++ HomeWork/task2.js
@@ -1,5 +1,35 @@
 let date = new Date();
+let result = date.getDay();
+let day;
 
 document.querySelector('#date').innerHTML = date;
-
 console.log(date);
+
+if (result == 0) {
+    day = "Воскрксенье"
+    out();
+} else if (result == 1) {
+    day = "Понедельник"
+    out();
+} else if (result == 2) {
+    day = "Вторник"
+    out();
+} else if (result == 3) {
+    day = "Среда"
+    out();
+} else if (result == 4) {
+    day = "Четверг"
+    out();
+} else if (result == 5) {
+    day = "Пятница"
+    out();
+} else {
+    day = "Суббота"
+    out();
+}
+
+function out() {
+    console.log(day);
+    document.querySelector('#day').innerHTML = day;
+}
+
--- HomeWork/task3.js
+++ HomeWork/task3.js
@@ -1,5 +1,21 @@
-document.querySelector('#btn').onclick = function () {
-    let output = document.querySelector('#output');
-    output.innerHTML = 'Привіт Андрій';
-    output.style.color = 'red';
-};
+inputYear();
+
+function inputYear() {
+    let year = prompt('Введите год (Целое число):');
+
+    if (isNaN(year)) {
+        alert('Это не число' + '\n' + 'Попробуйте снова:')
+        inputYear();
+    } else {
+        if (year % 1 !== 0) {
+            alert('Число не целое' + '\n' + 'Попробуйте снова:')
+            inputYear();
+        } else {
+            if(year % 400 === 0 || (year % 4 ===0 && year % 100 !== 0)){
+                alert('Это высосный год')
+            } else{
+                alert('Это не высокосный год')
+            }
+        }
+    }
+}
