851db1547cb4cf90399118e36edacc7bfda6bbd1
Ruslan Bihun <bigun.ruslan@gmail.com>
2024-08-01 11:45:08 +0200

    add counter in index.html and main.js

--- HomeWorkAnswers/Lesson4/Task1/JS/main.js
+++ HomeWorkAnswers/Lesson4/Task1/JS/main.js
@@ -1,18 +1,18 @@
 window.addEventListener("load", () => {
   let btn = document.querySelector("#btn");
-  // let blokComp = document.querySelector(".block_comp");
-  // let blockUser = document.querySelector(".block_user");
   let userChoiceBlock = document.querySelector(".forUserCHoice");
   let user = document.querySelector("#user");
   let comp = document.querySelector("#comp");
-
+  let compResult = document.querySelector("#compResult");
+  let userResult = document.querySelector("#userResult");
+  let userCount = 0;
+  let compCount = 0;
   let userIndexChoice, compIndexChoice;
 
   userChoiceBlock.addEventListener("click", (e) => {
     let adress = e.target.src;
     user.src = adress;
     userIndexChoice = getNumberOfImage(adress);
-   
   });
 
   btn.addEventListener("click", () => {
@@ -22,16 +22,20 @@ window.addEventListener("load", () => {
     compIndexChoice = getNumberOfImage(srcImg);
     setTimeout(() => {
       let result = getWinner(compIndexChoice, userIndexChoice);
-     if(result ===1){
-       comp.src = "./IMG/loser.jpg";
-       user.src = "./IMG/winner.jpg";
-     }else if(result === 0){
-      comp.src = "./IMG/winner.jpg";
-      user.src = "./IMG/loser.jpg";
-     }else{
-      comp.src = "./IMG/try.jpg";
-      user.src = "./IMG/try.jpg";
-     }
+      if (result === 1) {
+        comp.src = "./IMG/loser.jpg";
+        user.src = "./IMG/winner.jpg";
+        userCount ++;
+        userResult.innerHTML = userCount;
+      } else if (result === 0) {
+        comp.src = "./IMG/winner.jpg";
+        user.src = "./IMG/loser.jpg";
+        compCount ++;
+        compResult.innerHTML = compCount;
+      } else {
+        comp.src = "./IMG/try.jpg";
+        user.src = "./IMG/try.jpg";
+      }
     }, 1000);
   });
 });
