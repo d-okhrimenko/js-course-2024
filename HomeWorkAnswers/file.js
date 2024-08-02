5484935c0ef38a8ae3289cf6db622abfc67a6617
Tetiana Karpenko <kuzia2016@i.ua>
2024-08-02 17:52:24 +0300

    complete task 1

---
+++ HomeWorkAnswers/file.js
@@ -0,0 +1,9 @@
+    let number = prompt("Введіть число");
+    number = Number(number);
+    console.log(number);
+
+    if(number % 2 > 0) {
+        alert("Число не парне");
+    } else {
+        alert("Число парне");
+        }
