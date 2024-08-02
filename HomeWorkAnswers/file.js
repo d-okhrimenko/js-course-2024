cd8dfb0ef451e7ea751be95a6156b0f52d3886ab
Tetiana Karpenko <kuzia2016@i.ua>
2024-08-02 18:13:45 +0300

    complete task 2

--- HomeWorkAnswers/file.js
+++ HomeWorkAnswers/file.js
@@ -1,3 +1,5 @@
+    // Task 1
+
     let number = prompt("Введіть число");
     number = Number(number);
     console.log(number);
@@ -7,3 +9,30 @@
     } else {
         alert("Число парне");
         }
+
+        // Task 2
+
+    let currentDay = new Date().getDay();
+    console.log(currentDay);
+
+    if(currentDay === 1) {
+        alert("Блін, тільки понеділок");
+    }
+    if(currentDay === 2) {
+        alert("Хм, вівторок");
+    }
+    if(currentDay === 3) {
+        alert("Вже середа");
+    }
+    if(currentDay === 4) {
+        alert("Скоро...четвер");
+    }
+    if(currentDay === 5) {
+        alert("Ура Пятниця");
+    }
+    if(currentDay === 6) {
+        alert("Ура Субота");
+    }
+    if(currentDay === 0) {
+        alert("Неділя");
+    }
