04bb345391a385d394524a805b84a8b539cee724
Oleksandr <mario880001@gmail.com>
2024-08-06 22:05:55 +0300

    fixed the switch method

--- HomeWorkAnswears/Lesson 3/js/myhw3_task2.js
+++ HomeWorkAnswears/Lesson 3/js/myhw3_task2.js
@@ -1,51 +1,59 @@
 alert(new Date());
-console.log(new Date());
+console.log(new Date().getFullYear());
 let day = new Date().getDay();
 console.log(day);
 
-document.querySelector(".dayWeek").onclick = function() {
-    if(day == 0) {
-        alert("Неділя");
-    }
-    if(day == 1) {
-        alert("Понеділок");
-    }
-    if(day == 2) {
-        alert("Вівторок");
-    }
-    if(day == 3) {
-        alert("Середа");
-    }
-    if(day == 4) {
-        alert("Четвер");
-    }
-    if(day == 5) {
-        alert("П'ятниця");
-    }
-    if(day == 6) {
-        alert("Субота");
-    }
-}
+//if else if
+// document.querySelector(".dayWeek").onclick = function () {
+//     if (day == 0) {
+//         alert("Неділя");
+//     } else if (day == 1) {
+//         alert("Понеділок");
+//     } else if (day == 2) {
+//         alert("Вівторок");
+//     } else if (day == 3) {
+//         alert("Середа");
+//     } else if (day == 4) {
+//         alert("Четвер");
+//     } else if (day == 5) {
+//         alert("П'ятниця");
+//     } else if (day == 6) {
+//         alert("Субота");
+//     } else if (day == 7) {
+//         alert("Щось пішло не так!!!")
+//     }
+// }
 
-let a = "Сьогодні ";
-if(day == 0) {
-    alert(a + "Неділя");
-}
-if(day == 1) {
-    alert(a + "Понеділок");
-}
-if(day == 2) {
-    alert(a + "Вівторок");
-}
-if(day == 3) {
-    alert(a + "Середа");
-}
-if(day == 4) {
-    alert(a + "Четвер");
-}
-if(day == 5) {
-    alert(a + "П'ятниця");
-}
-if(day == 6) {
-    alert(a + "Субота");
+
+//switch
+document.querySelector(".dayWeek").onclick = function () {
+    switch (day) {
+        case 0:
+        alert("НЕДІЛЯ");
+        break;
+
+        case 1:
+        alert("ПОНЕДІЛОК");
+        break;
+
+        case 2:
+        alert("ВІВТОРОК");
+        break;
+
+        case 3:
+        alert("СЕРЕДА");
+        break;
+
+        case 4:
+        alert("ЧЕТВЕР");
+        break;
+
+        case 5:
+        alert("П'ЯТНИЦЯ");
+        break;
+
+        case 6:
+        alert("СУБОТА");
+        break;
+    }
 }
