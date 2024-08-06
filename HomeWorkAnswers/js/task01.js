d3fab9b400cee45e0f13504f42b7842af9b8a920
slavon213 <slavon213@gmail.com>
2024-08-06 18:49:43 +0300

    game is working

--- HomeWorkAnswers/js/task01.js
+++ HomeWorkAnswers/js/task01.js
@@ -5,14 +5,45 @@ function gameStart() {
         3: "Папір",
     };
 
-    let userAnswer = parseInt(
+    let userInput = parseInt(
         prompt(
             'Введіть цифри: 1 — для "камінь", 2 — для ножиці, 3 — для "папір"'
         )
     );
-    if (!isNaN(userAnswer) && userAnswer > 0 && userAnswer < 4) {
-        let pcAnswer = Math.floor(Math.random() * 3) + 1;
+    if (!isNaN(userInput) && userInput > 0 && userInput < 4) {
+        let pcInput = Math.floor(Math.random() * 3) + 1;
+        let userAnswer = things[userInput];
+        let pcAnswer = things[pcInput];
+        console.log(userAnswer);
         console.log(pcAnswer);
+
+        if (userAnswer === pcAnswer) {
+            alert("Нічия!");
+        } else {
+            if (userAnswer === things[1]) {
+                if (pcAnswer === things[2]) {
+                    alert("Ви виграли");
+                } else {
+                    alert("Комп'ютер виграв");
+                }
+            } else {
+                if (userAnswer === things[2]) {
+                    if (pcAnswer === things[3]) {
+                        alert("Ви виграли");
+                    } else {
+                        alert("Комп'ютер виграв");
+                    }
+                } else {
+                    if (userAnswer === things[3]) {
+                        if (pcAnswer === things[1]) {
+                            alert("Ви виграли");
+                        } else {
+                            alert("Комп'ютер виграв");
+                        }
+                    }
+                }
+            }
+        }
     } else {
         alert("Введіть тільки цифри з умови");
     }
