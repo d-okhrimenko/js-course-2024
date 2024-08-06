4feb01cfc460e1ec70ebd8127bcf940e8c4def1d
slavon213 <slavon213@gmail.com>
2024-08-06 17:38:57 +0300

    solution from Dmytro

--- HomeWorkAnswers/js/task_03.js
+++ HomeWorkAnswers/js/task_03.js
@@ -1,18 +1,8 @@
 function isLeapYear() {
     let userInput = parseInt(prompt("Введіть рік"));
     if (!isNaN(userInput) && userInput > 0) {
-        // Якщо рік без остачі ділиться на 4, то може бути високосним
-        if (userInput % 4 === 0) {
-            //  Якщо рік не ділиться без остачі на 100, то може бути високосним
-            if (userInput % 100 === 0) {
-                // Якщо рік без остачі не ділиться на 100 але ділиться без остачі на 400, то може бути високосним
-                if (userInput % 400 === 0) {
-                    alert(`Рік "${userInput}" високосний`);
-                }
-
-            } else {
-                alert(`Рік "${userInput}" високосний`);
-            }
+        if(userInput % 4 === 0 && (userInput % 100 !== 0 || userInput % 400 === 0)) {
+            alert(`Рік "${userInput}" високосний`);
         } else {
             alert(`Рік "${userInput}" не високосний`);
         }
@@ -20,4 +10,3 @@ function isLeapYear() {
         alert("Введіть рік більший від 0");
     }
 };
-
