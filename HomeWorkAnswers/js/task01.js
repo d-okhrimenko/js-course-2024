7d5d399e3070e079834485fda388e14e26d1fd15
slavon213 <slavon213@gmail.com>
2024-08-07 10:59:35 +0300

    task 01 change cycle to while and add exit from cycle

--- HomeWorkAnswers/js/task01.js
+++ HomeWorkAnswers/js/task01.js
@@ -15,49 +15,57 @@ function gameStart() {
     if (isNaN(countRound) || countRound <= 0) {
         alert("Введіть кількість раундів цифрами  і більше від нуля");
     } else {
-        for (let i = 1; i < countRound + 1; i++) {
-            let userInput = parseInt(
-                prompt(
-                    'Введіть цифри: 1 — для "камінь", 2 — для ножиці, 3 — для "папір"'
-                )
+        while (countRound > 0) {
+            let userInput = prompt(
+                'Введіть цифри: 1 — для "камінь", 2 — для ножиці, 3 — для "папір", "вихід" — для закінчення гри'
             );
-            if (!isNaN(userInput) && userInput > 0 && userInput < 4) {
-                let pcInput = Math.floor(Math.random() * 3) + 1;
-                let userAnswer = things[userInput];
-                let pcAnswer = things[pcInput];
-                playedRound++;
-                if (userAnswer === pcAnswer) {
-                    userCount++;
-                    pcCount++;
-                    alert(
-                        `Нічия! Ви вибрали "${userAnswer}", комп'ютер вибрав "${pcAnswer}".
-                        Раунд №: ${playedRound}, 
-                        К-сть ваших очок: "${userCount}", к-сть очок комп'ютера: "${pcCount}"`
-                    );
-                } else {
-                    if (
-                        (userAnswer === things[1] && pcAnswer === things[2]) ||
-                        (userAnswer === things[2] && pcAnswer === things[3]) ||
-                        (userAnswer === things[3] && pcAnswer === things[1])
-                    ) {
+            if (userInput.toLowerCase() === "вихід") {
+                break;
+            } else {
+                userInput = parseInt(userInput);
+                if (!isNaN(userInput) && userInput > 0 && userInput < 4) {
+                    let pcInput = Math.floor(Math.random() * 3) + 1;
+                    let userAnswer = things[userInput];
+                    let pcAnswer = things[pcInput];
+                    playedRound++;
+                    if (userAnswer === pcAnswer) {
                         userCount++;
-                        alert(
-                            `Ви виграли! Ви вибрали "${userAnswer}", комп'ютр вибрав "${pcAnswer}" .
-                        Раунд №: ${playedRound}, 
-                        К-сть ваших очок: "${userCount}", к-сть очок комп'ютера: "${pcCount}"`
-                        );
-                    } else {
                         pcCount++;
                         alert(
-                            `Комп'ютер виграв! Ви вибрали "${userAnswer}", комп'ютер вибрав "${pcAnswer}" .
-                        Раунд №: ${playedRound}, 
-                        К-сть ваших очок: "${userCount}", к-сть очок комп'ютера: "${pcCount}"`
+                            `Нічия! Ви вибрали "${userAnswer}", комп'ютер вибрав "${pcAnswer}".
+                            Раунд №: ${playedRound}, 
+                            К-сть ваших очок: "${userCount}", к-сть очок комп'ютера: "${pcCount}"`
                         );
+                    } else {
+                        if (
+                            (userAnswer === things[1] &&
+                                pcAnswer === things[2]) ||
+                            (userAnswer === things[2] &&
+                                pcAnswer === things[3]) ||
+                            (userAnswer === things[3] && pcAnswer === things[1])
+                        ) {
+                            userCount++;
+                            alert(
+                                `Ви виграли! Ви вибрали "${userAnswer}", комп'ютр вибрав "${pcAnswer}" .
+                            Раунд №: ${playedRound}, 
+                            К-сть ваших очок: "${userCount}", к-сть очок комп'ютера: "${pcCount}"`
+                            );
+                        } else {
+                            pcCount++;
+                            alert(
+                                `Комп'ютер виграв! Ви вибрали "${userAnswer}", комп'ютер вибрав "${pcAnswer}" .
+                            Раунд №: ${playedRound}, 
+                            К-сть ваших очок: "${userCount}", к-сть очок комп'ютера: "${pcCount}"`
+                            );
+                        }
                     }
+                    countRound--;
+                } else {
+                    alert('Введіть тільки цифри або "вихід" з умови');
                 }
-            } else {
-                alert("Введіть тільки цифри з умови");
             }
         }
+
+        alert("Гру закінчено");
     }
 }
