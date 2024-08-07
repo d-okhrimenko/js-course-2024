6d04173cbfe0914e5240f773f4c7e461584004bb
slavon213 <slavon213@gmail.com>
2024-08-07 09:55:11 +0300

    task1 part 1 improve if-else

--- HomeWorkAnswers/js/task01.js
+++ HomeWorkAnswers/js/task01.js
@@ -14,34 +14,24 @@ function gameStart() {
         let pcInput = Math.floor(Math.random() * 3) + 1;
         let userAnswer = things[userInput];
         let pcAnswer = things[pcInput];
-        console.log(userAnswer);
-        console.log(pcAnswer);
 
         if (userAnswer === pcAnswer) {
-            alert("Нічия!");
+            alert(
+                `Нічия! Ви вибрали "${userAnswer}", комп'ютер вибрав "${pcAnswer}"`
+            );
         } else {
-            if (userAnswer === things[1]) {
-                if (pcAnswer === things[2]) {
-                    alert("Ви виграли");
-                } else {
-                    alert("Комп'ютер виграв");
-                }
+            if (
+                (userAnswer === things[1] && pcAnswer === things[2]) ||
+                (userAnswer === things[2] && pcAnswer === things[3]) ||
+                (userAnswer === things[3] && pcAnswer === things[1])
+            ) {
+                alert(
+                    `Ви виграли! Ви вибрали "${userAnswer}", комп'ютр вибрав "${pcAnswer}"`
+                );
             } else {
-                if (userAnswer === things[2]) {
-                    if (pcAnswer === things[3]) {
-                        alert("Ви виграли");
-                    } else {
-                        alert("Комп'ютер виграв");
-                    }
-                } else {
-                    if (userAnswer === things[3]) {
-                        if (pcAnswer === things[1]) {
-                            alert("Ви виграли");
-                        } else {
-                            alert("Комп'ютер виграв");
-                        }
-                    }
-                }
+                alert(
+                    `Комп'ютер виграв! Ви вибрали "${userAnswer}", комп'ютер вибрав "${pcAnswer}"`
+                );
             }
         }
     } else {
