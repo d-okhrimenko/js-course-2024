8a8aae1454cb456950c5210cd9e70ddd9c17ebb4
Volodymyr-Leon <lvv.mvd@gmail.com>
2024-08-07 14:05:15 +0300

    HomeWorkAnswers_Lesson1_3

---
+++ HomeWorkAnswers/Lesson2/script.js
@@ -0,0 +1,5 @@
+document.querySelector("#btn").onclick = function() {
+    let output = document.querySelector("#output");
+    output.innerHTML = "Привіт Володимир";
+    output.style.color = "red";
+}
