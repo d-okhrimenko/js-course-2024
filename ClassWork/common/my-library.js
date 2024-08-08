521058b635454606f0d7fe3d2e5432cb950d8687
Volodymyr <iva12345@gmail.com>
2024-08-08 17:11:58 +0300

    Lesson 5 added

--- ClassWork/common/my-library.js
+++
@@ -1,7 +0,0 @@
-function getNumericValue(message) {
-    while (true) {
-        let value = prompt(message);
-        let isValid = !isNaN(value) && value !== "" && value !== null;
-        if (isValid) return Number(value);
-    }
-}
