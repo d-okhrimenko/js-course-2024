fb59a3a3feb9b77cb9d689668040f1e9f69f2213
Max-Myanovskiy <myanovskiy.m@gmail.com>
2024-08-09 08:28:03 +0300

    made refactoring Lesson5/task1/main.js

--- HomeWorkAnswers/Lesson5/task1/main.js
+++ HomeWorkAnswers/Lesson5/task1/main.js
@@ -1,24 +1,22 @@
 let myWeight = Number(prompt("Введіть вашу вагу?"));
 let myHeight = Number(prompt("Введіть ваш зріст?"));
+let myCategory;
 
-function indexWeight(myWeight, myHeight) {
-  let myIndexWeight = parseFloat((myWeight / (myHeight * myHeight)).toFixed(2));
-  return myIndexWeight;
-}
+let indexWeight = (myWeight, myHeight) =>
+  (myWeight / Math.pow(myHeight, 2)).toFixed(2);
 
-function categoryIndex(myIndexWeight) {
-  let myCategory;
-  if (myIndexWeight < 18.5) {
+let categoryIndex = (indexWeight) => {
+  if (indexWeight < 18.5) {
     myCategory = "Свідчить про недостатню вагу";
-  } else if (myIndexWeight >= 18.5 && myIndexWeight <= 24.9) {
+  } else if (indexWeight >= 18.5 && indexWeight <= 24.9) {
     myCategory = "Еквівалент нормальної маси тіла";
-  } else if (myIndexWeight >= 25 && myIndexWeight <= 29.9) {
+  } else if (indexWeight >= 25 && indexWeight <= 29.9) {
     myCategory = "Вказує на наявність зайвої ваги";
-  } else if (myIndexWeight > 30) {
+  } else if (indexWeight > 30) {
     myCategory = "Є ознакою ожиріння";
   }
   return myCategory;
-}
+};
 
 let indexIMT = indexWeight(myWeight, myHeight);
 let categoryFat = categoryIndex(indexIMT);
