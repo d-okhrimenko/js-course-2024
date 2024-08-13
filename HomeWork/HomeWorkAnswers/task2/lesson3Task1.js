2d2db9a57798a3bf1cd3eb4d8a93e0e58c1f31aa
Serhii <github0072024>
2024-08-13 22:08:05 +0300

    modified:   HomeWork/HomeWorkAnswers/task2/lesson3Task1.js         new file:   HomeWork/lesson3Task2.html         new file:   HomeWork/lesson3Task2.js

--- HomeWork/HomeWorkAnswers/task2/lesson3Task1.js
+++ HomeWork/HomeWorkAnswers/task2/lesson3Task1.js
@@ -1,11 +1,10 @@
 "use strict";
-"use strict";
 
 let value = prompt("Введіть число");
 let valueNumb = Number(value);
 
 if (value == valueNumb) {
-  if (valueNumb % 2 == 0) {
+  if (valueNumb % 2 === 0) {
     alert("це число парне");
   } else {
     alert("це число непарне");
