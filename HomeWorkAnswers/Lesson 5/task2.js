cbc5d1a2cff9e3ec6c19f9741264c4197cf58b82
mvn2004 <mvn2004@mail.ru>
2024-08-14 11:29:40 +0300

    Lesson 5

--- HomeWorkAnswers/Lesson 5/task2.js
+++ HomeWorkAnswers/Lesson 5/task2.js
@@ -1,14 +1,14 @@
 let getCircleArea = radius => (Math.PI * Math.pow(radius, 2)).toFixed(2);
 let getCirclePerimeter = radius => (2 * Math.PI * radius).toFixed(2);
 
-let getRectangleArea = (length, width) => length * width;
-let getRectanglePerimeter = (length, width) => 2 * (length + width);
+let getRectangleArea = (length, width) => (length * width).toFixed(2);
+let getRectanglePerimeter = (length, width) => (2 * (length + width)).toFixed(2);
 
 let getTriangleArea = (a, b, c) => {
 	let p = (a + b + c) / 2;
 	return (Math.sqrt(p * (p - a) * (p - b) * (p - c))).toFixed(2);
 }
-let getTrianglePerimeter = (a, b, c) => a + b + c;
+let getTrianglePerimeter = (a, b, c) => (a + b + c).toFixed(2);
 let figureType = 0;
 
 do {
@@ -23,25 +23,25 @@ const getAreaPerimeter = figureType => {
 	switch (figureType) {
 		case 1:
 			const radius = getNumericValue('Введіть радіус');
-			circleArea = getCircleArea(radius);
-			circlePerimeter = getCirclePerimeter(radius);
+			const circleArea = getCircleArea(radius)
+			const circlePerimeter = getCirclePerimeter(radius)
 			return `Площа кола: ${circleArea}, периметр кола: ${circlePerimeter}`;
 		case 2:
 			const length = getNumericValue('Введіть довжину прямокутника');
 			const width = getNumericValue('Введіть ширину прямокутника');
-			rectangleArea = getRectangleArea(length, width);
-			rectanglePerimeter = getRectanglePerimeter(length, width);
+			const rectangleArea = getRectangleArea(length, width)
+			const rectanglePerimeter = getRectanglePerimeter(length, width)
 			return `Площа прямокутника: ${rectangleArea}, периметр прямокутника: ${rectanglePerimeter}`;
 		case 3:
 			const a = getNumericValue('Введіть першу сторону трикутника');
 			const b = getNumericValue('Введіть другу сторону трикутника');
 			const c = getNumericValue('Введіть третю сторону трикутника');
-			triangleArea = getTriangleArea(a, b, c);
-			trianglePerimeter = getTrianglePerimeter(a, b, c);
+			const triangleArea = getTriangleArea(a, b, c)
+			const trianglePerimeter = getTrianglePerimeter(a, b, c)
 			return `Площа трикутника: ${triangleArea}, периметр трикутника: ${trianglePerimeter}`;
 	}
 }
 
 let areaPerimeter = getAreaPerimeter(figureType);
 
-alert(AreaPerimeter);
+alert(areaPerimeter);
