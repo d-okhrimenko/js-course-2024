62fd77ba73d7a8a50400f2f036154d16fc02f25e
Andrey Sham <avshamatrin@gmail.com>
2024-08-15 14:44:00 +0300

    Add Lesson6 / Task 1

---
+++ HomeWorkAnswers/task1.js
@@ -0,0 +1,24 @@
+function calculateAverageScore(...arrayEvaluations) {
+    let countEvaluations = arrayEvaluations.length;
+    let sum = 0;
+    for (const element of arrayEvaluations) {
+        sum += element
+    }
+    return sum / countEvaluations;
+}
+
+let maxScore = (...arrayEvaluations) => Math.max(...arrayEvaluations);
+
+let minScore = (...arrayEvaluations) => Math.min(...arrayEvaluations);
+
+let studentEvaluations = [];
+
+let userInput = prompt("Введіть оцінки через пробіл").trim().split(' ');
+userInput.forEach(element => {
+    let current = parseInt(element);
+    studentEvaluations.push(current);
+});
+
+alert(`Середній бал - ${calculateAverageScore(...studentEvaluations)}\n
+        Найвища оцінка - ${maxScore(...studentEvaluations)}\n
+        Найнижча оцінка - ${minScore(...studentEvaluations)}`)
