9da081e243379fa4af1d9544d2d11ede043f1ea1
tymofii-s <t967887744@gmail.com>
2024-08-19 16:42:48 +0300

    task 2 new version

--- HomeWorkAnswers/task2/script.js
+++ HomeWorkAnswers/task2/script.js
@@ -1,10 +1,12 @@
 let shoppingList = []
 
-function addCommodity(commodity) {
+function addCommodity() {
+    let commodity = prompt("Введіть продукт:")
     shoppingList.push(commodity)
 }
 
-function deleteCommodity(commodity) {
+function deleteCommodity() {
+    let commodity = prompt("Введіть продукт:")
     let index = shoppingList.indexOf(commodity)
     shoppingList.splice(index, 1)
 }
@@ -12,17 +14,3 @@ function deleteCommodity(commodity) {
 function showCommodity() {
     alert(shoppingList)
 }
-
-function getOperation() {
-    let operation = prompt("Вибаріть дію. Доступні дії: додавання товару, видалення товару, виведення списку");
-    if (operation === "додавання товару") {
-        let commodity = prompt("Введіть товар який хочете додати:")
-        addCommodity(commodity)
-    } else if (operation === "видалення товару") {
-        let commodity = prompt("Введіть товар який хочете видалити:")
-        deleteCommodity(commodity)
-    } else if (operation === "виведення списку") {
-        showCommodity()
-    }
-}
-getOperation()
