7685fb20f4e7d70e7d8ef667f14bf2460ff95506
immoBuz <buzic@ukr.net>
2024-08-25 21:46:51 +0300

    task 1 completed.

---
+++ HomeWorkAnswers/lesson 5/JS/modules/common.js
@@ -0,0 +1,14 @@
+
+let getEnteredValue = (msg, isTextAnswer = false) => {
+    while (true) {
+      let value = prompt(msg);
+      if(value !== "" && value !== null) {
+        if(isTextAnswer)  return value.toLowerCase();
+        else if(!isNaN(value)) return Number(value);
+        alert("Не коректне значення!");
+      }
+       
+      // if (!isNaN(value) && value !== "" && value !== null) return Number(value);
+      // alert("Не коректне значення!");
+    }
+  }
--- HomeWorkAnswers/lesson 5/JS/task1.js
+++ HomeWorkAnswers/lesson 5/JS/task1.js
@@ -1,22 +1,13 @@
-let getBMI = (mass, height) => (mass / Math.pow((height/100), 2)).toFixed(2);
+let getBMI = (mass, height) => (mass / Math.pow(height / 100, 2)).toFixed(2);
 
-let getBMICategory = bmi => {
-    if(bmi < 18.5) return `Недостатня вага: ІМТ = ${bmi}`;
-    else if(bmi >= 30) return `Ожиріння: ІМТ = ${bmi}`;
-    else if(bmi <= 24.9) return `Нормальна вага: ІМТ = ${bmi}`;
-    else return `Надмірна вага: ІМТ = ${bmi}`;
-}
+let getBMICategory = (bmi) => {
+  if (bmi < 18.5) return `Недостатня вага: ІМТ = ${bmi}`;
+  else if (bmi >= 30) return `Ожиріння: ІМТ = ${bmi}`;
+  else if (bmi <= 24.9) return `Нормальна вага: ІМТ = ${bmi}`;
+  else return `Надмірна вага: ІМТ = ${bmi}`;
+};
 
-let getNumericValue = msg => {
-    while (true) {
-      let value = prompt(msg);
-      if (!isNaN(value) && value !== "" && value !== null) return Number(value);
-      alert("Не коректне значення!");
-    }
-  }
+let bodyMass = getEnteredValue("Введіть значення ваги (кг): ");
+let bodyHeight = getEnteredValue("Введіть значення зросту (см): ");
 
-  let bodyMass = getNumericValue("Введіть значення ваги (кг): ");
-  let bodyHeight = getNumericValue("Введіть значення зросту (см): ");
-
-  let bmiCategory = getBMICategory(getBMI(bodyMass, bodyHeight));
-  alert(bmiCategory);
+alert(getBMICategory(getBMI(bodyMass, bodyHeight)));
