1a1cd2e1d683ff478657b22535a1d91b00999275
immoBuz <buzic@ukr.net>
2024-08-27 01:02:50 +0300

    task 2 completed.

--- HomeWorkAnswers/lesson 5/JS/task2.js
+++ HomeWorkAnswers/lesson 5/JS/task2.js
@@ -1,85 +1,66 @@
-let getCircleArea = (radius) => Math.PI * Math.pow(radius, 2);
-let getCirclePerimeter = (radius) => 2 * Math.PI * radius;
-
-let getRectangleArea = (length, width) => length * width;
-let getRectanglePerimeter = (length, width) => 2 * (length + width);
-
-let getTriangleArea = (fstSide, sndSide, thdSide) => {
-  let p = (fstSide + sndSide + thdSide) / 2;
-  return Math.sqrt(p * (p - fstSide) * (p - sndSide) * (p + thdSide));
-};
-let getTrianglePerimeter = (fstSide, sndSide, thdSide) =>
-  fstSide + sndSide + thdSide;
-
-const figures = ["коло", "прямокутник", "трикутник"];
+// масиви з показниками для оформлення програми
+const figuresNames = ["коло", "прямокутник", "трикутник"];
 const figuresParams = [
-    ["радіус"],
-    ["довжина", "ширина"],
-    ["1-й бік", "2-й бік", "3-й бік"]
+  ["радіус"],
+  ["довжина", "ширина"],
+  ["1-й бік", "2-й бік", "3-й бік"],
 ];
+const figureCharacteristics = ["площа", "периметр"];
 
-let getFiguresString = () => {
+// обчислення характеристик кола
+let getCircleArea = (paramsArr) => Math.PI * Math.pow(paramsArr[0], 2);
+let getCirclePerimeter = (paramsArr) => 2 * Math.PI * paramsArr[0];
+// обчислення характеристик прямокутника
+let getRectangleArea = (paramsArr) => paramsArr[0] * paramsArr[1];
+let getRectanglePerimeter = (paramsArr) => 2 * (paramsArr[0] + paramsArr[1]);
+// обчислення характеристик трикутника
+let getTriangleArea = (paramsArr) => {
+  let p = (paramsArr[0] + paramsArr[1] + paramsArr[2]) / 2;
+  return Math.sqrt(p * (p - paramsArr[0]) * (p - paramsArr[1]) * (p - paramsArr[2]));
+};
+let getTrianglePerimeter = (paramsArr) => paramsArr[0] + paramsArr[1] + paramsArr[2];
+
+// допоміжні функції
+
+// отримуємо представлення доступних фігур
+let getFiguresString = () => { 
   let str = "";
-
-  for (let i = 0; i < figures.length; i++) {
-    str += "'" + figures[i].charAt(0).toUpperCase() + figures[i].slice(1) + "'";
-    if (i < figures.length - 1) str += ",";
-    else str += "?";
-  }
-
+  for (let i = 0; i < figuresNames.length; i++) 
+    str += `'${figuresNames[i].charAt(0).toUpperCase()}${figuresNames[i].slice(1)}'${((i < figuresNames.length - 1) ? ", " : "?")}`;
   return str;
 };
+// перевіряємо чи введена фігура дозволена
+let isProperFigure = (figure) => figuresNames.includes(figure); 
+// подаємо користувачу результати
+let reportFigureCharacteristics = (figIdx, getArea, getPerimeter, figParams) => 
+  `${figuresNames[figIdx]} :\n` +
+  `${figureCharacteristics[0]} = ${(getArea(figParams)).toFixed(2)}\n` +
+  `${figureCharacteristics[1]} = ${(getPerimeter(figParams)).toFixed(2)}`;
+// отримуємо масив введених користувачем значень параметрів фігури
+let getFigureParamsArray = (figIdx) => {
+  let figureParamsArray = [];
+  let sndIdx = 0;
+  for (let i = 0; i < figIdx + 1; i++)
+    figureParamsArray.push(getEnteredValue(`Введіть ${figuresNames[figIdx]} : ${figuresParams[figIdx][sndIdx++]}`));
+  return figureParamsArray;
+}
 
-let isProperFigure = (figure) => figures.includes(figure);
-
-console.log(getFiguresString());
 
 let figure = getEnteredValue(`Оберіть фігуру: ${getFiguresString()}`, true);
-console.log(figure);
-console.log(figures.includes(figure));
+
 if (isProperFigure(figure)) {
-    let param, temp = 0;
-    let figureIndex = figures.indexOf(figure);
-    switch (figureIndex) {
-        case 0: // коло
-            temp = 0;
-            console.log(temp);
-            param = getEnteredValue(`Введіть ${figures[figureIndex]} : ${figuresParams[figureIndex][temp++]}`, true);
-            console.log(param);
-
-            // let figureParams = new Array();
-            // for (let i = 0; i < figureIndex + 1; i++) {
-            //     let param = getEnteredValue(`Введіть ${figures[figureIndex]} : ${figuresParams[figureIndex][i]}`, true);
-            //     figureParams.push(param);
-            // }
-            // for (i = 0; i < figureIndex + 1; i++)
-            //     console.log(figureParams[i]);
-            break;
-        case 1:
-            console.log(temp);
-            param = getEnteredValue(`Введіть ${figures[figureIndex]} : ${figuresParams[figureIndex][temp++]}`, true);
-            console.log(param);
-            console.log(temp);
-            param = getEnteredValue(`Введіть ${figures[figureIndex]} : ${figuresParams[figureIndex][temp++]}`, true);
-            console.log(param);
-
-            break;
-        case 2:
-            console.log(temp);
-            param = getEnteredValue(`Введіть ${figures[figureIndex]} : ${figuresParams[figureIndex][temp++]}`, true);
-            console.log(param);
-            console.log(temp);
-            param = getEnteredValue(`Введіть ${figures[figureIndex]} : ${figuresParams[figureIndex][temp++]}`, true);
-            console.log(param);
-            console.log(temp);
-            param = getEnteredValue(`Введіть ${figures[figureIndex]} : ${figuresParams[figureIndex][temp++]}`, true);
-            console.log(param);
-            break;
-    }
+  let figureIndex = figuresNames.indexOf(figure);
+  let figureParams = getFigureParamsArray(figureIndex);
+  
+  switch (figureIndex) {
+    case 0: // коло
+      alert(reportFigureCharacteristics(figureIndex, getCircleArea, getCirclePerimeter, figureParams));
+      break;
+    case 1: // прямокутник
+      alert(reportFigureCharacteristics(figureIndex, getRectangleArea, getRectanglePerimeter, figureParams));
+      break;
+    case 2: // трикутник
+      alert(reportFigureCharacteristics(figureIndex, getTriangleArea, getTrianglePerimeter, figureParams));
+      break;
+  }
 }
-// const radius = getNumericValue("Введіть радіус:");
-// let circleArea = getCircleArea(radius);
-// let circlePerimeter = getCirclePerimeter(radius);
-
-// console.log(circleArea);
-// console.log(circlePerimeter);
