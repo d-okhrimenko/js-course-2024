8ffa2a7763a2e3634786d82a028a02118a2790ed
Max-Myanovskiy <myanovskiy.m@gmail.com>
2024-08-27 14:58:30 +0300

    finished Lesson7/task1

---
+++ HomeWorkAnswers/Lesson7/task1/main.js
@@ -0,0 +1,70 @@
+class StudentObject {
+  constructor(id, firstName, lastName, age, averageScore) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.averageScore = averageScore;
+  }
+}
+
+let students = {
+  group: [],
+
+  add: function (id, firstName, lastName, age, averageScore) {
+    let newStudent = new StudentObject(
+      id,
+      firstName,
+      lastName,
+      age,
+      averageScore
+    );
+    this.group.push(newStudent);
+  },
+
+  remove: function (id) {
+    let index = this.group.findIndex((student) => student.id === id);
+    if (index !== -1) {
+      this.group.splice(index, 1);
+    }
+  },
+
+  display: function () {
+    if (this.group.length === 0) {
+      console.log("Список студентів порожній.");
+    } else {
+      console.log("Список студентів:");
+      this.group.forEach(function (student) {
+        console.log("------------------------");
+        console.log(`ID: ${student.id}`);
+        console.log(`Имя: ${student.firstName}`);
+        console.log(`Фамилия: ${student.lastName}`);
+        console.log(`Возраст: ${student.age}`);
+        console.log(`Средний балл: ${student.averageScore}`);
+        console.log("------------------------");
+      });
+    }
+  },
+
+  find: function (id) {
+    let foundStudent = null;
+    for (let i = 0; i < this.group.length; i++) {
+      if (this.group[i].id === id) {
+        foundStudent = this.group[i];
+        break;
+      }
+    }
+
+    if (!foundStudent) {
+      console.log(`Студента з ID: ${id} не знайдено!`);
+    } else {
+      console.log("------------------------");
+      console.log(`ID: ${foundStudent.id}`);
+      console.log(`Ім'я: ${foundStudent.firstName}`);
+      console.log(`Прізвище: ${foundStudent.lastName}`);
+      console.log(`Вік: ${foundStudent.age}`);
+      console.log(`Середній бал: ${foundStudent.averageScore}`);
+      console.log("------------------------");
+    }
+  },
+};
