9b71a3930cd074881fab6778c6d06dd39081b91b
slavon213 <slavon213@gmail.com>
2024-08-27 14:51:56 +0300

    innit Lesson7

---
+++ HomeWorkAnswers/js/task01.js
@@ -0,0 +1,8 @@
+class Student {
+    constructor(id, name, lastName, age, average) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.average = average;
+    }
+}
