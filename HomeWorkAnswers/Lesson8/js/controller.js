abc9d08dec8b486c22ee1f66318080c31def42d9
Petro Kovalchuk <pkovalchuk@outlook.com>
2024-08-31 23:20:45 +0300

    Add function render in view.js

--- HomeWorkAnswers/Lesson8/js/controller.js
+++ HomeWorkAnswers/Lesson8/js/controller.js
@@ -1,4 +1,4 @@
-let html = document.querySelector("#viewBooks");
+let html = document.querySelector("#bookList");
 let template = document.querySelector("#template").innerHTML;
-//contactList.render(contactBook.contacts, html);
-renderBooks.renderWithTemplate(dataBooks, html, template);
+//renderBooks.renderWithTemplate(dataBooks, html, template);
+renderBooks.render(dataBooks, html, template);
