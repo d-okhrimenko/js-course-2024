1e8fecf963710a96dd1c9a9dedecc2bde03fc877
bohdanshvetc <dan6147@ukr.net>
2024-09-01 11:18:44 +0300

    Home Work Lesson4

--- Lesson3/file_3.js
+++ Lesson3/file_3.js
@@ -1,12 +1,12 @@
-console.log("index_3.html")
+console.log('index_3.html')
 
-const button = document.querySelector("#btn")
+const button = document.querySelector('#btn')
 
-button.addEventListener('click', function() {
-  let userInput = document.querySelector("#input").value
-  if(isNaN(userInput) || userInput.trim() == "") {
-    alert("Це не число!")
+button.addEventListener('click', function () {
+  let userInput = document.querySelector('#input').value
+  if (isNaN(userInput) || userInput.trim() == '') {
+    alert('Це не число!')
   } else {
-    userInput % 4 == 0 ? alert("Високосний рік") : alert("НЕ Високосний рік")
+    userInput % 4 == 0 ? alert('Високосний рік') : alert('НЕ Високосний рік')
   }
 })
