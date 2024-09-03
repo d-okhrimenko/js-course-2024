2e1282e17199e577207aee3275c1bd70d0ade48c
olvak <82042917+olvak@users.noreply.github.com>
2024-09-03 17:51:30 +0300

    add homework 8 solution

---
+++ HomeWorkAnswers/Homework Lesson 8/js/model.js
@@ -0,0 +1,34 @@
+const bookInfo = {
+  books: [
+    {
+      "title": "The Mysterious Island",
+      "author": "Jules Verne",
+      "year": "1875",
+      "genre": "Adventure Novel"
+    },
+    {
+      "title": "Forrest Gump",
+      "author": "Winston Groom",
+      "year": "1986",
+      "genre": "Novel"
+    },
+    {
+      "title": "White Fang",
+      "author": "Jack London",
+      "year": "1906",
+      "genre": "Adventure"
+    },
+    {
+      "title": "The Witcher",
+      "author": "Andrzej Sapkowski",
+      "year": "1986",
+      "genre": "Fantasy"
+    },
+    {
+      "title": "Ivanhoe",
+      "author": "Walter Scott",
+      "year": "1819",
+      "genre": "Chivalric Romance"
+    }
+  ],
+};
---
+++ HomeWorkAnswers/Homework Lesson 8/js/view.js
@@ -0,0 +1,21 @@
+const bookList = {
+  render(books, element) {
+    books.forEach(book => {
+      let section = document.createElement("section");
+      section.classList.add("book");
+      section.insertAdjacentHTML("beforeend", `<h2>Title: ${book.title}</h2>`);
+      section.insertAdjacentHTML("beforeend", `<h3>Author: ${book.author}</h3>`);
+      section.insertAdjacentHTML("beforeend", `<p>Year: ${book.year}</p>`);
+      section.insertAdjacentHTML("beforeend", `<p>Genre: ${book.genre}</p>`);
+      element.appendChild(section);
+    });
+  },
+
+  renderWithTemplate(books, element, template) {
+    let html = "";
+    books.forEach(book => {
+      html += Mustache.render(template, book);
+    });
+    element.innerHTML = html;
+  },
+};
