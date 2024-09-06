553485ca7ebcf7826e34a456d45d6d1501e43528
Vitiok2021 <viktorpts1vol@gmail.com>
2024-09-06 20:50:52 +0300

    change all

--- HomeWorkAnswers/controller.js
+++ HomeWorkAnswers/controller.js
@@ -1,8 +1,11 @@
 // 1.   ================================
-let list = document.querySelector('.book-list__items')
+// let list = document.querySelector('.book-list__items')
 
-bookView.render(bookModule.books, list)
+// bookView.render(bookModule.books, list)
 
 // 2.   ================================
+let template = document.querySelector('#template').innerHTML
+let listItems = document.querySelector('.book-list__items')
 
+bookView.renderWithTemplate(bookModule.books, listItems, template)
 // 3.   ================================
