d6fb1e869ac7e2d7de5c9c3a0d2c7581e0bec710
RBM <roma19176>
2024-09-08 15:02:28 -0500

    html

--- HomeWorkAnswers-Lesson3/script.js
+++ HomeWorkAnswers-Lesson3/script.js
@@ -1,12 +1,1 @@
-document.querySelector("#btn-data").onclick = function () {
-	let output = document.querySelector("#output-data");
-	// console.log(new Date);
-	alert(new Date);
-};
-
-
-document.querySelector("#btn").onclick = function () {
-	let btnH = document.querySelector("#btn");
-	btnH.innerHTML = 'Привіт Роман'
-	
-};
+let errorMessage = "file not found";
