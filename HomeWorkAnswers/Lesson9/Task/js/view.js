2b5b71e2ffe8588cb3c479002ce7bb00a6cd3035
Andre <andrey.lobanov@outlook.com>
2024-09-08 23:14:37 +0200

    del obsolete func renderBook

--- HomeWorkAnswers/Lesson9/Task/js/view.js
+++ HomeWorkAnswers/Lesson9/Task/js/view.js
@@ -8,16 +8,5 @@ const bookList = {
         });
 
         element.innerHTML = html;
-    },
-
-    renderBook({ id, title, year, author, genre }) {
-        return `
-        <button onclick="edit(${id})">Редагувати</button>
-        <button onclick="remove(${id})">Видалити</button>
-        <h2>${title}</h2>
-        <p>Рік: ${year}</p>
-        <p>Автор: ${author}</p>
-        <p>Жанр: ${genre}</p>
-        `;
     }
 }
