245f8c30b65fa36c741990026c189f94c728b134
GrinAlex2023 <numizmat2005@gmail.com>
2024-09-08 21:10:32 +0300

    HomeWork_09

---
+++ HomeWorkAnswers/Lesson9/js/controller.js
@@ -0,0 +1,54 @@
+const html = document.querySelector("#book-list");
+const template = document.querySelector("#template").innerHTML;
+const bookForm = document.querySelector("#bookForm");
+
+// inputs
+const titleInput = document.querySelector("#titleInput");
+const authorInput = document.querySelector("#authorInput");
+const yearInput = document.querySelector("#yearInput");
+const genreInput = document.querySelector("#genreInput");
+
+let editId = null;
+
+function render() {
+  bookView.renderWithTemplate(bookList.books, html, template);
+}
+
+bookForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let book = {
+    title: titleInput.value,
+    author: authorInput.value,
+    year: yearInput.value,
+    genre: genreInput.value,
+  };
+
+  if (editId == null) {
+    bookList.add(book);
+  } else {
+    bookList.update(editId, book);
+    editId = null;
+  }
+  render();
+});
+
+function remove(id) {
+  bookList.remove(id);
+  render();
+}
+
+function edit(id) {
+  const book = bookList.find(id);
+
+  editId = book.id;
+
+  titleInput.value = book.title;
+  authorInput.value = book.author;
+  yearInput.value = book.year;
+  genreInput.value = book.genre;
+
+  render();
+}
+
+render();
