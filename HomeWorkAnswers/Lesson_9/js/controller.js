6a0ac631c0f2953f227b3b4998ca46cfd3c3d245
Petro Shchesniuk <petro.shchesniuk-ki211@nung.edu.ua>
2024-09-09 17:19:10 +0300

    Lesson 9 is done

---
+++ HomeWorkAnswers/Lesson_9/js/controller.js
@@ -0,0 +1,47 @@
+const html = document.querySelector("#bookList");
+const template = document.querySelector("#template").innerHTML;
+const bookForm = document.querySelector("#bookForm");
+
+const titleInput = document.querySelector("#titleInput");
+const authorInput = document.querySelector("#authorInput");
+const yearInput = document.querySelector("#yearInput");
+const genreInput = document.querySelector("#genreInput");
+
+let editId = null;
+
+function render() { bookList.renderWithTemplate(booksList.books, html, template); }
+
+bookForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let book = {
+        title: titleInput.value,
+        author: authorInput.value,
+        year: yearInput.value,
+        genre: genreInput.value
+    }
+    if(editId==null){    booksList.add(book);
+    } else {booksList.update(editId,book)
+        editId = null;
+    }
+    render();
+})
+
+function remove(id) {
+    booksList.remove(id);
+    render();
+}
+
+function edit(id) {
+    const book = booksList.find(id);
+    editId = book.id;
+
+    titleInput.value = book.title;
+    authorInput.value = book.author;
+    yearInput.value = book.year;
+    genreInput.value = book.genre;
+
+    render();
+}
+
+render();
