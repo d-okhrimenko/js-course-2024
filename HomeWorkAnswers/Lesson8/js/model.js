fe2e830edd980f714d30d99558b48d394a5cada1
JuVerh <yverkhovaya@gmail.com>
2024-09-09 10:28:20 +0300

    Lesson8

---
+++ HomeWorkAnswers/Lesson8/js/model.js
@@ -0,0 +1,34 @@
+const booksList = {
+  books: [
+    {
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      year: 1960,
+      genre: "Fiction",
+    },
+    {
+      title: "Pride and Prejudice",
+      author: "Jane Austen",
+      year: 1813,
+      genre: "Romance",
+    },
+    {
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      year: 1925,
+      genre: "Tragedy",
+    },
+    {
+      title: "Moby-Dick",
+      author: "Herman Melville",
+      year: 1851,
+      genre: "Adventure",
+    },
+    {
+      title: "War and Peace",
+      author: "Leo Tolstoy",
+      year: 1869,
+      genre: "Historical",
+    },
+  ],
+};
