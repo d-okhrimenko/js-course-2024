da9eab5a2891b0b3bf06a59a625cb442d70aa182
JuVerh <yverkhovaya@gmail.com>
2024-09-10 15:03:50 +0300

    Add id's to model.js

--- HomeWorkAnswers/Lesson9/js/model.js
+++ HomeWorkAnswers/Lesson9/js/model.js
@@ -1,30 +1,35 @@
 const booksList = {
   books: [
     {
+      id: 1,
       title: "To Kill a Mockingbird",
       author: "Harper Lee",
       year: 1960,
       genre: "Fiction",
     },
     {
+      id: 2,
       title: "Pride and Prejudice",
       author: "Jane Austen",
       year: 1813,
       genre: "Romance",
     },
     {
+      id: 3,
       title: "The Great Gatsby",
       author: "F. Scott Fitzgerald",
       year: 1925,
       genre: "Tragedy",
     },
     {
+      id: 4,
       title: "Moby-Dick",
       author: "Herman Melville",
       year: 1851,
       genre: "Adventure",
     },
     {
+      id: 5,
       title: "War and Peace",
       author: "Leo Tolstoy",
       year: 1869,
