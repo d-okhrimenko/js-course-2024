df52e5aab255f77791452a48c6880b2c2f0a8723
Valeriia Moskalenko <moskalenkovvy@gmail.com>
2024-09-13 18:16:59 +0300

    HT for lesson 2 done

---
+++ HomeWork/moskalenko/lesson02/script.js
@@ -0,0 +1,4 @@
+document.querySelector("#btn-hello").onclick = function() {
+            let output = document.querySelector("#p-hello-output");
+            output.innerHTML = "Привіт Валерія";
+        }
