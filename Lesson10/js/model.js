feecf4cc8fe6f0e9589ea380e281e42beb2442a5
bohdanshvetc <dan6147@ukr.net>
2024-09-13 18:48:45 +0300

    Home Work Lesson10

---
+++ Lesson10/js/model.js
@@ -0,0 +1,66 @@
+const allBooks = {
+  books: [
+    { 
+      id: 1,
+      title: "1984", 
+      author: "Джордж Орвелл", 
+      year: 1949, 
+      genre: "Антиутопія" 
+    },
+
+    { 
+      id: 2,
+      title: "Гаррі Поттер і філософський камінь", 
+      author: "Дж. К. Роулінг", 
+      year: 1997, 
+      genre: "Фентезі" 
+    },
+
+    { 
+      id: 3,
+      title: "Гра престолів", 
+      author: "Джордж Р. Р. Мартін", 
+      year: 1996, 
+      genre: "Фентезі" 
+    },
+
+    { 
+      id: 4,
+      title: "Старий і море", 
+      author: "Ернест Хемінгуей", 
+      year: 1952, 
+      genre: "Пригодницький роман" 
+    },
+
+    { 
+      id: 5,
+      title: "Майстер і Маргарита", 
+      author: "Михайло Булгаков", 
+      year: 1967, 
+      genre: "Фантастика" 
+    },
+  ],
+
+  lastId: 5,
+
+  add(book) {
+    this.lastId++;
+    book.id = this.lastId;
+    this.books.push(book);
+  },
+
+  remove(id) {
+    let index = this.books.findIndex(b => b.id == id);
+    this.books.splice(index, 1);
+  },
+
+  update(id, book) {
+    let index = this.books.findIndex(b => b.id == id);
+    this.books[index] = book;
+  },
+
+  find(id) {
+    let index = this.books.findIndex(b => b.id == id);
+    return this.books[index];
+  }
+}
