7ec9628f1c8bc2f38184cb4fd12195ab73b87640
Valeriia Moskalenko <moskalenkovvy@gmail.com>
2024-09-14 18:40:48 +0300

    Task 2 done

--- HomeWork/moskalenko/Task2.js
+++ HomeWork/moskalenko/Task2.js
@@ -1,3 +1,45 @@
+function calcCircle(r) {
+    let P = 2 * Math.PI * r;
+    let S = Math.PI * Math.pow(r, 2);
+    return {
+        'P': P,
+        'S': S
+    }
+}
+
+function calcRecrangle(a, b) {
+    let P = 2 * (a + b);
+    let S = a * b;
+    return {
+        'P': P,
+        'S': S
+    }
+}
+
+function calcTriangle(a, b, c) {
+    let P = a + b + c;
+    let p = P / 2;
+    let S = Math.pow((p * (p - a) * (p - b) * (p - c)), 0.5);
+    return {
+        'P': P,
+        'S': S
+    }
+}
+
 document.querySelector("#btn-t2").onclick = function () {
-    
+    let res;
+    if (document.querySelector("#t2-circle").checked) {
+        let r = parseFloat(prompt("Радіус кола="))
+        res = calcCircle(r)
+    } else if (document.querySelector("#t2-rect").checked) {
+        let a = parseFloat(prompt("Сторона прямокутника 1="))
+        let b = parseFloat(prompt("Сторона прямокутника 2="))
+        res = calcRecrangle(a, b)
+    } else if (document.querySelector("#t2-triag").checked) {
+        let a = parseFloat(prompt("Сторона трикутника 1="))
+        let b = parseFloat(prompt("Сторона трикутника 2="))
+        let c = parseFloat(prompt("Сторона трикутника 3="))
+        res = calcTriangle(a, b, c)
+    }
+    alert(`P = ${res["P"]}\nS = ${res["S"]}`)
 }
