4bfb67c40608ea18fba56c6680ba568574db622e
Andrey Urusov <urusov1402@gmail.com>
2024-09-14 21:30:49 +0300

    add home work 9

---
+++ HomeWork/MyHomeWork9/js/view.js
@@ -0,0 +1,19 @@
+const bookList = {
+    render(books, element) {
+        element.innerHTML = '';
+
+        books.forEach(book => {
+            const li = document.createElement('li');
+
+            li.classList.add('book-block');  
+            li.insertAdjacentHTML('beforeend', `<h2>${book.title}</h2>`);
+            li.insertAdjacentHTML('beforeend', `<h3>${book.author}</h3>`);
+            li.insertAdjacentHTML('beforeend', `<p>${book.year}</p>`);
+            li.insertAdjacentHTML('beforeend', `<p>${book.genre}</p>`);
+            li.insertAdjacentHTML('beforeend', `<button class='btn' onclick='edit(${book.id})'>Редагувати</button>`);
+            li.insertAdjacentHTML('beforeend', `<button class='btn' onclick='remove(${book.id})'>Видалити</button>`);
+            element.appendChild(li);
+        });
+    },
+}
+
