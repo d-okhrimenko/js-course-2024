34691ccdddf7e2deb6faab20ec0c59d6a22f9f34
yelyzaveta.herasymenko <yelyzaveta.herasymenko@awwcor.com>
2024-09-16 14:37:31 +0300

    Les9. Added methods into models, functions into controller, styles

--- HomeWorkAnswers/Lesson9/JS/controller.js
+++ HomeWorkAnswers/Lesson9/JS/controller.js
@@ -1,3 +1,56 @@
-let bookHtml = document.querySelector("#book-list");
-let template = document.querySelector("#template").innerHTML;
-bookList.renderWithTemplate(bookAttributs.books, bookHtml, template);
+const bookHtml = document.querySelector("#book-list");
+const template = document.querySelector("#template").innerHTML;
+
+const bookForm = document.querySelector("#bookForm");
+
+//inputs
+const titleInput = document.querySelector("#titleInput");
+const authorInput = document.querySelector("#authorInput");
+const yearInput = document.querySelector("#yearInput");
+const genreInput = document.querySelector("#genreInput");
+
+let editId = null;
+
+function render() {
+  bookList.renderWithTemplate(bookAttributs.books, bookHtml, template);
+}
+
+bookForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  let book = {
+    title: titleInput.value,
+    author: authorInput.value,
+    year: yearInput.value,
+    genre: genreInput.value
+  };
+
+  if (editId == null) {
+    bookAttributs.add(book);
+  } else {
+    bookAttributs.update(editId, book);
+    editId = null;
+  }
+  
+  render();
+})
+
+function remove(id) {
+  bookAttributs.remove(id);
+  render();
+}
+
+function edit(id){
+  const book = bookAttributs.find(id);
+
+  editId = book.id;
+
+  titleInput.value = book.title,
+  authorInput.value = book.author,
+  yearInput.value = book.year,
+  genreInput.value = book.genre;
+
+  render();
+}
+
+render();
