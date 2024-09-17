7e25cb127bebc08483fff8196a26ef29e1f288f5
Inna Yasynchuk <i_bybyk@ukr.net>
2024-09-17 17:23:14 +0300

    Lesson 9 Done

--- HomeWorkAnswers/Lesson8/notes.js
+++ HomeWorkAnswers/Lesson8/notes.js
@@ -5,6 +5,9 @@ e.classList.add('test');
 e.classList.remove('test');
 e.classList.contains('test') // перевірити, чи є клас на елементі
 
+e.style.borderRadius = '8px';
+// -moz-radial-gradient =  MozRadialGradient
+
 let list = document.createDocumentFragment // можна створити фрагмент, згенерувати складну структуру і потім вставити в документ. Вставиться один раз, а не кожен раз додаючи новий елемент
 
 let li = document.createElement('li') // <li>
@@ -14,3 +17,6 @@ e.appendChild(li) // додати дочірній елемент
 e.insertAdjacentHTML('beforeend', <li>text2</li>) // вставляє код в вибрану частину коду відносно елементу (перед, після ...)
 
 e.insertAdjacentElement('afterbegin', li); // вставляє елемент
+
+e.addEventListener('click', functionName) // використ. замість e.onClick = (){}
+e.removeEventListener('click', functionName)
