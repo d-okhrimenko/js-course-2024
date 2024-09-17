742891bb5f70b759cec6e07f874540d7a660443e
immoBuz <buzic@ukr.net>
2024-09-18 00:37:58 +0300

    task is completed.

--- HomeWorkAnswers/lesson 7/JS/task01.js
+++ HomeWorkAnswers/lesson 7/JS/task01.js
@@ -31,10 +31,12 @@ let groupController = {
 
     showMsg: function (msg) { console.log(msg); },
 
-    showInfo: function (msgTemplateFn, id = -1) {
+    showInfo: function (id = -1) {
         let list = (id == -1) ? this.group : this.findById(id);
 
-        return list.map(element => this.showMsg(msgTemplateFn(element)));
+        console.log(getSymbolString('*', 20));
+
+        return list.map(element => this.showMsg(`id: ${element.id} | ${element.name} ${element.surname}: ${element.age} y.o. | avg.score: ${element.avgScore}`));
     },
 
     deleteStudent: function(id) {
@@ -45,21 +47,27 @@ let groupController = {
         let foundIndex = findIndex(id);
         if (foundIndex != -1) this.group.splice(foundIndex, 1);
     },
-
 };
 
+// ініціалізація групи студеїв
 groupController.addStudent("John", "Elbow", 21, 150);
 groupController.addStudent("Elsea", "Woker", 20, 178);
 groupController.addStudent("Ingvar", "Jahnsson", 22, 182);
 
-groupController.showInfo(x => `id: ${x.id} | ${x.name} ${x.surname}: ${x.age} y.o. | avg.score: ${x.avgScore}`);
-groupController.showInfo(x => `id: ${x.id} | ${x.name} ${x.surname}: ${x.age} y.o. | avg.score: ${x.avgScore}`, 1);
-
+// демонстрація групи студеїв
+groupController.showInfo();
+// демонстрація студея групи за його id
+groupController.showInfo(1);
+// вилучення стужея з групи за його id
 groupController.deleteStudent(0);
 
-groupController.showInfo(x => `id: ${x.id} | ${x.name} ${x.surname}: ${x.age} y.o. | avg.score: ${x.avgScore}`);
+// демонстрація групи студеїв
+groupController.showInfo();
 
+// додавання студея до групи
 groupController.addStudent("Icko", "Casiori", 21, 180);
-groupController.showInfo(x => `id: ${x.id} | ${x.name} ${x.surname}: ${x.age} y.o. | avg.score: ${x.avgScore}`, 3);
-groupController.showInfo(x => `id: ${x.id} | ${x.name} ${x.surname}: ${x.age} y.o. | avg.score: ${x.avgScore}`);
+// демонстрація студея групи за його id
+groupController.showInfo(3);
+// демонстрація групи студеїв
+groupController.showInfo();
 
