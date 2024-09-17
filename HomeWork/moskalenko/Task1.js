f69b4710c4dc6b57b2e6094d174887ba80b30d84
Valeriia Moskalenko <moskalenkovvy@gmail.com>
2024-09-17 23:40:23 +0300

    Task done

--- HomeWork/moskalenko/Task1.js
+++ HomeWork/moskalenko/Task1.js
@@ -1,27 +1,50 @@
 let library = {
+    bookIsCounter:  5,
     books: [
-        { title: "1984", author: "Джордж Орвелл", year: 1949, genre: "Антиутопія" },
-        { title: "Гаррі Поттер і філософський камінь", author: "Дж. К. Роулінг", year: 1997, genre: "Фентезі" },
-        { title: "Гра престолів", author: "Джордж Р. Р. Мартін", year: 1996, genre: "Фентезі" },
-        { title: "Старий і море", author: "Ернест Хемінгуей", year: 1952, genre: "Пригодницький роман" },
-        { title: "Майстер і Маргарита", author: "Михайло Булгаков", year: 1967, genre: "Фантастика" },
+        { id: 0, title: "1984", author: "Джордж Орвелл", year: 1949, genre: "Антиутопія" },
+        { id: 1, title: "Гаррі Поттер і філософський камінь", author: "Дж. К. Роулінг", year: 1997, genre: "Фентезі" },
+        { id: 2, title: "Гра престолів", author: "Джордж Р. Р. Мартін", year: 1996, genre: "Фентезі" },
+        { id: 3, title: "Старий і море", author: "Ернест Хемінгуей", year: 1952, genre: "Пригодницький роман" },
+        { id: 4, title: "Майстер і Маргарита", author: "Михайло Булгаков", year: 1967, genre: "Фантастика" },
     ],
+
+    add(book) {
+        book.id = this.bookIsCounter++;
+        this.books.push(book);
+    },
+
+    remove(bookId) {
+        let index = this.books.findIndex(x => x.id == bookId);
+        this.books.splice(index, 1);
+    },
+
+    update(bookId, book) {
+        let index = this.books.findIndex(x => x.id == bookId);
+        this.books[index] = book;
+    },
+
+    find(bookId) {
+        let index = this.books.findIndex(x => x.id == bookId);
+        return this.books[index];
+    },
 }
 
 let libraryUI = {
-    showBookCard: function(book, element) {
+    showBookCard: function (book, element) {
         let section = document.createElement("section");
-            section.classList.add("book");
-            section.insertAdjacentHTML("beforeend", `<h3>${book.title}</h3>`);
-            section.insertAdjacentHTML("beforeend", `<p>Автор: ${book.author}</p>`);
-            section.insertAdjacentHTML("beforeend", `<p>Рік: ${book.year}</p>`);
-            section.insertAdjacentHTML("beforeend", `<p>Жанр: ${book.genre}</p>`);
-            section.insertAdjacentHTML("beforeend", `<hr />`);
-            element.appendChild(section);
+        section.classList.add("book");
+        section.insertAdjacentHTML("beforeend", `<h3>${book.title}</h3>`);
+        section.insertAdjacentHTML("beforeend", `<p>Автор: ${book.author}</p>`);
+        section.insertAdjacentHTML("beforeend", `<p>Рік: ${book.year}</p>`);
+        section.insertAdjacentHTML("beforeend", `<p>Жанр: ${book.genre}</p>`);
+        section.insertAdjacentHTML("beforeend", `<button onclick="edit(${book.id})">Редагувати</button>`);
+        section.insertAdjacentHTML("beforeend", `<button onclick="remove(${book.id})">Видалити</button>`);
+        section.insertAdjacentHTML("beforeend", `<hr />`);
+        element.appendChild(section);
         return section;
     },
 
-    showBooks: function(books) {
+    showBooks: function (books) {
         let booksContainer = document.querySelector("#t1-books");
         books.forEach(book => {
             this.showBookCard(book, booksContainer);
@@ -30,4 +53,74 @@ let libraryUI = {
 
 }
 
-libraryUI.showBooks(library.books);
+function render() {
+    document.querySelector("#t1-books").innerHTML = '';
+    libraryUI.showBooks(library.books);
+}
+
+
+const bookForm = document.querySelector("#bookForm");
+
+const bookIdHidden = document.querySelector("#bookId");
+const titleInput = document.querySelector("#titleInput");
+const authorInput = document.querySelector("#authorInput");
+const yearInput = document.querySelector("#yearInput");
+const genreInput = document.querySelector("#genreInput");
+
+const bookFormCreateBtn = document.querySelector("#bookForm-new");
+const bookFormUpdateBtn = document.querySelector("#bookForm-upd");
+const bookFormClearBtn = document.querySelector("#bookForm-del");
+
+bookFormCreateBtn.onclick = function () {
+    let book = {
+        title: titleInput.value,
+        author: authorInput.value,
+        year: yearInput.value,
+        genre: genreInput.value,
+    };
+
+    library.add(book);
+
+    render();
+};
+
+bookFormUpdateBtn.onclick = function () {
+    let book = {
+        title: titleInput.value,
+        author: authorInput.value,
+        year: yearInput.value,
+        genre: genreInput.value,
+    };
+
+    library.update(bookIdHidden.value, book);
+    
+    render();
+};
+
+bookFormClearBtn.onclick = function () {
+    bookIdHidden.value = "";
+    titleInput.value = "";
+    authorInput.value = "";
+    yearInput.value = "";
+    genreInput.value = "";
+    bookFormUpdateBtn.setAttribute("disabled", "disabled");
+};
+
+function remove(id) {
+    library.remove(id);
+    render();
+}
+
+function edit(id) {
+    const book = library.find(id);
+
+    bookIdHidden.value = book.id;
+    titleInput.value = book.title;
+    authorInput.value = book.author;
+    yearInput.value = book.year;
+    genreInput.value = book.genre;
+
+    bookFormUpdateBtn.removeAttribute("disabled");
+}
+
+render();
