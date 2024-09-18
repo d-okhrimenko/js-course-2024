937d117892e37bed063ddc42c623b8ba0a555ed4
Andrey Urusov <urusov1402@gmail.com>
2024-09-18 07:32:34 +0300

    start home work lesson 10

--- ClassWork/Lesson 8/js/controller.js
+++
@@ -1,4 +0,0 @@
-let html = document.querySelector("#contact-list");
-let template = document.querySelector("#template").innerHTML;
-//contactList.render(contactBook.contacts, html);
-contactList.renderWithTemplate(contactBook.contacts, html, template);
