4c7692feebf9f076eddd523ae2e95138c23eea5a
Konstantin-Mazykin <konstantin.mazykin@gmail.com>
2024-09-23 11:59:00 +0300

    Added rounding pressure

--- HomeWorkAnswers/Lesson11/js/view.js
+++ HomeWorkAnswers/Lesson11/js/view.js
@@ -10,7 +10,7 @@ function render(data, htmlElement) {
     "beforeend",
     `<p>Поточна температура: ${data.main.temp}&#186;С (максимальна: ${data.main.temp_max}&#186;С, мінімальна: ${data.main.temp_min}&#186;С, відчуваєтся: ${data.main.feels_like}&#186;С)</p>`
   );
-  section.insertAdjacentHTML("beforeend", `<p>Тиск: ${Number(data.main.pressure) * 0.7501} мм рт. ст.</p>`);
+  section.insertAdjacentHTML("beforeend", `<p>Тиск: ${Math.round(Number(data.main.pressure) * 0.7501)} мм рт. ст.</p>`);
 
   htmlElement.appendChild(section);
 }
