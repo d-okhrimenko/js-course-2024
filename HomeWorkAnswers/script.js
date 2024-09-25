1983e20bb3128271aae2754f30802a25960b39eb
Bohdan Voitov <bohdan.voiyov@gmail.com>
2024-09-25 13:54:20 +0300

    done_HomeWorkAnswers#11

---
+++ HomeWorkAnswers/script.js
@@ -0,0 +1,42 @@
+// API ключ
+const apiKey = "a4c065f24d8b6403dc757b8eec2628c1";
+
+// Обробка натискання кнопки "Отримати погоду"
+document
+  .getElementById("getWeatherButton")
+  .addEventListener("click", getWeather);
+
+function getWeather() {
+  const city = document.getElementById("cityInput").value;
+
+  if (city === "") {
+    document.getElementById(
+      "weatherResult"
+    ).innerHTML = `<p>Будь ласка, введіть назву міста.</p>`;
+    return;
+  }
+
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=ua`;
+
+  // Використання fetch для запиту до API
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Місто не знайдено або проблема з сервером");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const weatherInfo = `
+                <h2>Погода у місті: ${data.name}</h2>
+                <p>Температура: ${data.main.temp} °C</p>
+                <p>Опис: ${data.weather[0].description}</p>
+            `;
+      document.getElementById("weatherResult").innerHTML = weatherInfo;
+    })
+    .catch((error) => {
+      document.getElementById(
+        "weatherResult"
+      ).innerHTML = `<p>${error.message}</p>`;
+    });
+}
