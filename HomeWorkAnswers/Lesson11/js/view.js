9e6bd3852228c961c570a199a004d5d0c58c6b2f
Konstantin-Mazykin <konstantin.mazykin@gmail.com>
2024-09-25 08:37:18 +0300

    Changing the text style and amount of information received

--- HomeWorkAnswers/Lesson11/js/view.js
+++ HomeWorkAnswers/Lesson11/js/view.js
@@ -20,9 +20,7 @@ function render(data, htmlElement) {
             <div class="${section}">
                 <h2>Погода у населеному пункті ${data.name} (${data.sys.country}) </h2>
                 <p>Погода: ${data.weather[0].description}</p>
-                <p>Поточна температура: ${data.main.temp}&#186;С (максимальна: ${data.main.temp_max}&#186;С, мінімальна: ${
-    data.main.temp_min
-  }&#186;С, відчуваєтся: ${data.main.feels_like}&#186;С)</p>
+                <p>Поточна температура: ${data.main.temp}&#186;С (відчуваєтся як: ${data.main.feels_like}&#186;С)</p>
                 <p>Тиск: ${Math.round(Number(data.main.pressure) * 0.7501)} мм рт. ст.</p>
             </div>
             `;
